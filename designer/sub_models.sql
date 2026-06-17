bc244cff89666f354eb6185ab749cbf7d616ce94
nvitelli <nicolo.vitelli1@gmail.com>
2026-06-17 17:16:57 +0200

    2026-06-17 17:16:57

--- designer/sub_models.sql
+++ designer/sub_models.sql
@@ -4,14 +4,14 @@ with ssm0 as (select * from snp_sub_model with read only)
 	select ssm0.i_smod as smod_no
 		,sm.mod_name
 		,ssm0.smod_name
-		,ssm0.smod_type
-		,ssm0.cod_smod
-		,ssm0.i_smod_parent
+		,coalesce(ssmp.smod_name,'Unspecified') as parent_smod_name
 		,to_char(ssm0.first_date, 'yyyy-mm-dd hh24:mi:ss') as first_deploy_ts
 		,to_char(ssm0.last_date, 'yyyy-mm-dd hh24:mi:ss') as last_deploy_ts
 	from ssm0
 		inner join sm
 			on ssm0.i_mod = sm.i_mod
+		left join ssm0 ssmp
+			on ssm0.i_smod_parent = ssmp.i_smod
 )
 select *
 from ssm
